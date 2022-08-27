39301244fda5571c61b177c4d0f530d321235d2f
anurag madhesia <madhesiaanurag99@gmail.com>
2022-08-27 15:58:13 +0530

    async-example

--- index.js
+++ index.js
@@ -1,16 +1,22 @@
-const fs = require('fs');
-const path = require('path');
-const dirPath= path.join(__dirname,'crud');
-const filePath = `${dirPath}/apple.txt`;
-fs.writeFileSync(filePath,'this is a simple file');
-fs.readFile(filePath,'utf8',(err,item)=>{
-console.log(item);
-})
-fs.appendFile(filePath,' for fruits',(err)=>{
-if(!err) console.log("file is updated")
-})
-fs.rename(filePath, `${dirPath}/fruit.txt`,(err)=>{
-if(!err) console.log("file name is updated")
+console.log('async-example1');
+let a = 5;
+let b = 0;
+
+// setTimeout(()=>{
+//     console.log('async-example2');
+//     b = 10;
+// },1000)
+
+let waitingData = new Promise((resolve,reject)=>{
+    setTimeout(()=>{
+        resolve(30);
+    },1000)
 })
 
-// fs.unlinkSync(`${dirPath}/fruit.txt`);
+console.log('async-example3');
+waitingData.then((data)=>{
+    console.log('async-example2');
+    b=data;
+    console.log(a + b);
+})
+console.log(a + b);
