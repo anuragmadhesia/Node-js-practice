928009166e6edb64f7da23b3e26ea524d6e88cd0
anurag madhesia <madhesiaanurag99@gmail.com>
2022-08-27 11:14:35 +0530

    fs-crud

--- index.js
+++ index.js
@@ -1,3 +1,16 @@
-const arr =[3,4,5,7,1,2,5,9];
-console.log(arr);
-console.warn(arr)
+const fs = require('fs');
+const path = require('path');
+const dirPath= path.join(__dirname,'crud');
+const filePath = `${dirPath}/apple.txt`;
+fs.writeFileSync(filePath,'this is a simple file');
+fs.readFile(filePath,'utf8',(err,item)=>{
+console.log(item);
+})
+fs.appendFile(filePath,' for fruits',(err)=>{
+if(!err) console.log("file is updated")
+})
+fs.rename(filePath, `${dirPath}/fruit.txt`,(err)=>{
+if(!err) console.log("file name is updated")
+})
+
+// fs.unlinkSync(`${dirPath}/fruit.txt`);
